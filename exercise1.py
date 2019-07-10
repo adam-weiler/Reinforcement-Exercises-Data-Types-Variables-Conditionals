a2e4c801f9d5bca25d763a2aadd97941540deaaa
adam-weiler <adam.weiler@yahoo.ca>
2019-07-10 15:37:29 -0400

    Code is asking user for input and providing recommendations.

--- exercise1.py
+++ exercise1.py
@@ -5,20 +5,28 @@ comedy = 'Airplane!'
 book = '1984'
 
 
-def ask_theme(theme):
+def ask_theme(theme): #Asks user which genre they prefer.
     print('Do you like {} movies?'.format(theme))
-    return input()
+    return input().lower()
+
+def give_recommendation(title, media="movie"): #Gives the user a recommendation based on their choices. Media is movie by default.
+    return 'You would enjoy the {} \'{}\'.'.format(media, title)
 
 
-def movie_recommendation():
+def title_recommendation(): #Asks user their preference on 3 genres. Based on their input, gives a recommendation.
     doc_input = ask_theme('Documentary')
     drama_input = ask_theme('Drama')
     comedy_input = ask_theme('Comedy')
 
-    print(doc_input)
-    print(drama_input)
-    print(comedy_input)
+    if (doc_input == "yes"):
+        print(give_recommendation(documentary))
+    elif (doc_input == "no") and (drama_input == "yes" and comedy_input == "yes"):
+        print(give_recommendation(dramedy))
+    elif (drama_input == "yes"):
+        print(give_recommendation(drama))
+    elif (comedy_input == "yes"):
+        print(give_recommendation(comedy))
+    else:
+        print(give_recommendation(book, 'book'))
 
-
-
-movie_recommendation()
+title_recommendation() #Calls the main function.
